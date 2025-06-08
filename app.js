72348c25a467072991e4e1289963f8cd812c3790
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2025-06-08 21:53:23 +0200

    js

--- app.js
+++ app.js
@@ -5,3 +5,39 @@ console.log(button);
 const liste = document.getElementById('favoris_list')
 console.log(liste);
 
+button.addEventListener("click" , () => {
+    const valeur = input.value.trim();
+    console.log(valeur);
+    if (valeur === '') {
+        alert("Veuillez entrer une valeur")
+        return;
+    }
+
+    const li = document.createElement('li')
+
+    li.textContent = valeur;
+
+    const buttonSupprimer = document.createElement('button')
+    buttonSupprimer.textContent = 'x'
+    buttonSupprimer.classList.add('supprimer')
+    buttonSupprimer.addEventListener("click", () => {
+        liste.removeChild(li);
+    })
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked === true) {
+            li.classList.add('done')
+        } else {
+            li.classList.remove('done')
+        }
+    })
+
+    li.appendChild(buttonSupprimer);
+    li.prepend(checkbox)
+    liste.appendChild(li)
+
+    input.value = ''
+    input.focus();
+    
+})
